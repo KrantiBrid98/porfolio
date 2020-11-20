8ca0ad109dc9a3c787dd0bc86aa84f211eccb988
KrantiBrid98 <45895279+KrantiBrid98@users.noreply.github.com>
2020-11-20 13:29:28 +0530

    changed email

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -2,10 +2,8 @@ import React from "react";
 import "./contact.css";
 
 const Contact = () => {
-    const click = () => {
-        console.log(`click`)
-        window.open('mailto:test@example.com');
-    }
+    const click = () =>  window.open('mailto:krantibrid@gmail.com');
+    
     return <div className="block-wrapper" style={{backgroundColor: "#252934"}} id="cont-section">
         <h3 style={{color: "#fefefe", marginBottom: "20px"}}>Contact</h3>
             <p>Have a question or need further information? Send me an email. </p>
