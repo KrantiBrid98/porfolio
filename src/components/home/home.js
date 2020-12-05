fe8fec7c6e73395fda6067d1d16a0796f7e16ef3
KrantiBrid98 <45895279+KrantiBrid98@users.noreply.github.com>
2020-12-05 20:36:09 +0530

    changed href to skills-section

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div className="home-content" id="home-section">
             <div className="home-script">Hello, I am <span style={{color: `#e31b6d`, fontWeight: `bold`}}>Kranti Brid</span>.</div>
             <div className="home-script">I am a front-end developer</div>
-            <button><a href="#about-section">View my Work <span className="arrow">➡</span></a>
+            <button><a href="#skills-section">View my Work <span className="arrow">➡</span></a>
             </button>
         </div>
     </div>
