9baa0516536eb3129c089e9bb98696c03b31cae2
KrantiBrid98 <krantibrid@gmail.com>
2021-02-20 19:48:22 +0530

    added light theme

--- src/components/project/project.js
+++ src/components/project/project.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./project.css";
 import github from "../../assets/github.svg";
 import data from "../../content/data.json";
+import { ThemeContext } from '../context';
 
 let _ = require('lodash');
 
@@ -30,50 +31,53 @@ class Project extends React.Component {
             if (val.isSelected)
                 selectedLang = key;
         });
-        console.log(selectedLang)
-        return <div className="block-wrapper" id="proj-section">
-            <h3>Projects</h3>
-            <div style={{ textAlign: "center" }}>
-                <button className={this.state.all.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("all")}>All</button>
-                <button className={this.state.js.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("js")}>Javascript</button>
-                <button className={this.state.react.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("react")}>React</button>
-                <button className={this.state.node.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("node")}>Node</button>
-            </div>
-            <div className={"card-container"}>
-                {
-                    selectedLang !== `all` && data.projects.filter(project => project.tag === selectedLang).map((project, index) => <div className="flip-card" key={index}>
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front content">
-                                <div className="project-name">{project.name}</div>
-                                <div className="skills">{project.skills}</div>
-                            </div>
-                            <div className="flip-card-back content">
-                                <div className="description">{project.description}</div>
-                                {project.link && <a className="github" target="_blank" rel="noopener" href={project.link}></a>}
-                            </div>
+        return <ThemeContext.Consumer>
+            {
+                (value) => <div className={`block-wrapper block-wrapper2-color-darkMode_${value[0]}`} id="proj-section">
+                        <h3 className={`darkMode_${value[0]}`}>Projects</h3>
+                        <div style={{ textAlign: "center" }}>
+                            <button className={this.state.all.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("all")}>All</button>
+                            <button className={this.state.js.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("js")}>Javascript</button>
+                            <button className={this.state.react.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("react")}>React</button>
+                            <button className={this.state.node.isSelected ? "language-tag active" : "language-tag inactive"} onClick={() => this.onbuttonSelect("node")}>Node</button>
                         </div>
+                        <div className={"card-container"}>
+                            {
+                                selectedLang !== `all` && data.projects.filter(project => project.tag === selectedLang).map((project, index) => <div className="flip-card" key={index}>
+                                    <div className="flip-card-inner">
+                                        <div className={`flip-card-front content flip-card-front-darkMode_${value[0]}`}>
+                                            <div className={`project-name-darkMode_${value[0]}`}>{project.name}</div>
+                                            <div className={`skills-darkMode_${value[0]}`}>{project.skills}</div>
+                                        </div>
+                                        <div className="flip-card-back content">
+                                            <div className="description">{project.description}</div>
+                                            {project.link && <a className="github" target="_blank" rel="noopener" href={project.link}></a>}
+                                        </div>
+                                    </div>
+                                </div>
+                                )
+                            }
+                            {
+                                selectedLang === `all` && data.projects.map((project, index) => <div className="flip-card" key={index}>
+                                    <div className="flip-card-inner">
+                                        <div className={`flip-card-front content flip-card-front-darkMode_${value[0]}`}>
+                                            <div className={`project-name-darkMode_${value[0]}`}>{project.name}</div>
+                                            <div className={`skills-darkMode_${value[0]}`}>{project.skills}</div>
+                                        </div>
+                                        <div className="flip-card-back content">
+                                            <div className="description">{project.description}</div>
+                                            {project.link && <a className="github" target="_blank" rel="noopener" href={project.link}></a>}
+                                        </div>
+                                    </div>
+                                </div>
+                                )}
+                        </div>
+                        <><p>And many more on</p>
+                            <a className="github" target="_blank" rel="noopener" href="https://github.com/KrantiBrid98"></a>
+                        </>
                     </div>
-                    )
                 }
-                {
-                    selectedLang === `all` && data.projects.map((project, index) => <div className="flip-card" key={index}>
-                        <div className="flip-card-inner">
-                            <div className="flip-card-front content">
-                                <div className="project-name">{project.name}</div>
-                                <div className="skills">{project.skills}</div>
-                            </div>
-                            <div className="flip-card-back content">
-                                <div className="description">{project.description}</div>
-                                {project.link && <a className="github" target="_blank" rel="noopener" href={project.link}></a>}
-                            </div>
-                        </div>
-                    </div>
-                    )}
-            </div>
-            <><p>And many more on</p>
-                <a className="github" target="_blank" rel="noopener" href="https://github.com/KrantiBrid98"></a>
-            </>
-        </div>
+        </ThemeContext.Consumer>
     }
 }
 
