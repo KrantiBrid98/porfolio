f9e089f043551977c64612422b2d70397192aeba
KrantiBrid98 <45895279+KrantiBrid98@users.noreply.github.com>
2025-03-09 19:05:42 +0530

    Update skills.js

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -11,22 +11,24 @@ const Skills = () => {
       {(value) => {
         return (
         <div className={`block-wrapper block-wrapper1-color-darkMode_${value[0]}`} id="skills-section">
-          <h3 className={`darkMode_${value[0]}`}>Skills</h3>
-          <div className="skills-column">
-            <div className="row">
-              <img src={`${display}`} alt="Paris" width="302" height="375" />
-              <div className={`user-description darkMode_${value[0]}`}>{data.skills.description}</div>
-            </div>
-            <div className="row">
-              <h6 className={`darkMode_${value[0]}`}>Skills</h6>
-              <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-                {data.skills.skillset.map((skill, index) => {
-                  return (
-                    <div className="tags" key={index}>
-                      {skill}
-                    </div>
-                  );
-                })}
+          <div>
+            <h3 className={`darkMode_${value[0]}`}>Skills</h3>
+            <div className="skills-column">
+              <div className="row">
+                <img src={`${display}`} alt="Paris" width="302" height="375" />
+                <div className={`user-description darkMode_${value[0]}`}>{data.skills.description}</div>
+              </div>
+              <div className="row">
+                <h6 className={`darkMode_${value[0]}`}>Skills</h6>
+                <div style={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {data.skills.skillset.map((skill, index) => {
+                    return (
+                      <div className="tags" key={index}>
+                        {skill}
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             </div>
           </div>
